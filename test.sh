39fe19e9f26f13e580cefb09daecb0b75df7fc01
Guilherme Rodrigues Cunha <80539618+guilhermefmk@users.noreply.github.com>
2022-08-19 08:41:04 -0300

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# user = $1
-# senha = $2
-
-
-sshpass -p Vetorial20 ssh -o ServerAliveInterval=3 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null administrator@172.16.112.139 "powershell"
-sshpass -p Vetorial20 ssh -o ServerAliveInterval=3 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null administrator@172.16.112.139 "Import-Module ActiveDirectory"
-sshpass -p Vetorial20 ssh -o ServerAliveInterval=3 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null administrator@172.16.112.139 "Get-ADUser -Filter * -SearchScope Subtree –SearchBase “CN=Guilherme Cunha,OU=Vetorial,DC=ad,DC=teste,DC=local” | Ft Name,sAMAccountName"
-sshpass -p Vetorial20 ssh -o ServerAliveInterval=3 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null administrator@172.16.112.139 "Exit"
